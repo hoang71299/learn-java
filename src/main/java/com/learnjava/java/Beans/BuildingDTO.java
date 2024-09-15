21d5e1376155e5e563ec2b371f17ec94984ac628
huynhhoang <huynhhoang712003@gmail.com>
2024-09-16 00:42:07 +0700

    PathVariable and RestController

--- src/main/java/com/learnjava/java/Beans/BuildingDTO.java
+++ src/main/java/com/learnjava/java/Beans/BuildingDTO.java
@@ -6,16 +6,32 @@ public class BuildingDTO {
     private String ward;
     private String street;
 
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Integer getNumberOfBasement() {
+        return numberOfBasement;
     }
 
     public void setNumberOfBasement(Integer numberOfBasement) {
         this.numberOfBasement = numberOfBasement;
     }
 
+    public String getWard() {
+        return ward;
+    }
+
     public void setWard(String ward) {
         this.ward = ward;
+    }
+
+    public String getStreet() {
+        return street;
     }
 
     public void setStreet(String street) {
--- src/main/java/com/learnjava/java/api/BuildingAPI.java
+++ src/main/java/com/learnjava/java/api/BuildingAPI.java
@@ -2,20 +2,21 @@ package com.learnjava.java.api;
 
 import java.util.Map;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.learnjava.java.Beans.BuildingDTO;
 
-@Controller
+@RestController
 public class BuildingAPI {
 
-    @RequestMapping(value = "/api/building/", method = RequestMethod.GET)
-    @ResponseBody
+    // @RequestMapping(value = "/api/building/", method = RequestMethod.GET)
+    @GetMapping(value = "/api/building/") // cu phap ngan gon
     public BuildingDTO getBuilding(@RequestParam(value = "name", required = false) String nameBuilding,
             @RequestParam(value = "numberOfBasement", required = false) Integer numberOfBasement,
             @RequestParam(value = "ward", required = false) String ward) {
@@ -27,8 +28,16 @@ public class BuildingAPI {
         // System.out.println(nameBuilding + " " + numberOfBasement);
     }
 
-    @RequestMapping(value = "/api/building/", method = RequestMethod.POST)
+    // @RequestMapping(value = "/api/building/", method = RequestMethod.POST)
+    @PostMapping(value = "/api/building/") // cu phap ngan gon
     public void getBuilding2(@RequestBody BuildingDTO buildingDTO) {
         System.out.println("ok2");
+    }
+
+    @DeleteMapping(value = "/api/building/{id}/{name}")
+    public void deleteBuilding(@PathVariable Integer id, @PathVariable String name,
+            @RequestParam(value = "ward", required = false) String ward) {
+        System.out.println("đã xóa nhà có id là " + id + " roi nhe");
+
     }
 }
